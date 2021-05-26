00cd053389de5cd7a8d005c7af86192e1980a995
王建朝 <wangjianchao1@jd.com>
2021-05-26 09:24:44 +0800

    Initial commit

---
+++ src/main/java/io/github/tinyking/scaffold/SpringBootFreemarkerScaffoldApplication.java
@@ -0,0 +1,13 @@
+package io.github.tinyking.scaffold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFreemarkerScaffoldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFreemarkerScaffoldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/tinyking/scaffold/SpringBootFreemarkerScaffoldApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tinyking.scaffold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFreemarkerScaffoldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
